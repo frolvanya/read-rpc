2c2368242dcf27625b2c85c42c7cacf5cc08c4b4
Ivan Frolov <frolvanya@gmail.com>
2024-06-09 18:58:17 -0400

    chore: WIP

--- rpc-server/src/modules/clients/methods.rs
+++ rpc-server/src/modules/clients/methods.rs
@@ -29,20 +29,37 @@ pub async fn next_light_client_block(
         near_jsonrpc::primitives::types::light_client::RpcLightClientNextBlockRequest::parse(
             params,
         )?;
-    match data
-        .near_rpc_client
-        .call(request, Some("next_light_client_block"))
-        .await?
+
+    // TODO: remove unwrap
+    let block_height = data
+        .db_manager
+        .get_block_by_hash(request.last_block_hash, "next_light_client_block")
+        .await
+        .unwrap();
+
+    // TODO: remove unwrap
+    let last_block = near_lake_framework::s3_fetchers::fetch_block(
+        &data.s3_client,
+        &data.s3_bucket_name,
+        block_height,
+    )
+    .await
+    .unwrap();
+
+    let last_epoch_id = last_block.header.epoch_id;
+    let last_next_epoch_id = last_block.header.next_epoch_id;
+    let last_height = last_block.header.height;
+
+    // TODO: remove unwrap
+    let final_block = crate::utils::get_final_block(&data.near_rpc_client, false)
+        .await
+        .unwrap();
+
+    if last_epoch_id == final_block.header.epoch_id
+        || last_next_epoch_id == final_block.header.epoch_id
     {
-        Some(light_client_block) => Ok(
-            near_jsonrpc::primitives::types::light_client::RpcLightClientNextBlockResponse {
-                light_client_block: Some(std::sync::Arc::new(light_client_block)),
-            },
-        ),
-        None => Ok(
-            near_jsonrpc::primitives::types::light_client::RpcLightClientNextBlockResponse {
-                light_client_block: None,
-            },
-        ),
+        todo!()
     }
+
+    todo!()
 }
